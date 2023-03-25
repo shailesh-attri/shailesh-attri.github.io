4cfe95f97124b21e5c50c047a0a8b1d07a739fef
shailesh-attri <127712044+shailesh-attri@users.noreply.github.com>
2023-03-25 19:06:17 +0530

    Hello

--- script.js
+++ script.js
@@ -10,4 +10,38 @@
   function call(){
     alert('Please wait...');
   }
+  function submit(){
+    alert('Your appointment has been Booked');
+  }
+
+
+  window.addEventListener('scroll', function() {
+    var navbar = document.getElementById('header');
+    if (window.scrollY > 0) {
+      navbar.classList.add('scrolled');
+    } else {
+      navbar.classList.remove('scrolled');
+    }
+  });
+
+  function toggleNav() {
+    const menuBtn = document.getElementById('menu-btn');
+    const navList = document.getElementById('navbar');
+    
+    menuBtn.classList.toggle('fa-bars');
+    menuBtn.classList.toggle('fa-bars');
+    navList.classList.toggle('show');
+    
+  }
+  
+  // function toggleNav() {
+  //   var x = document.getElementById("navbar");
+  //   if (x.style.display === "block") {
+  //     x.style.display = "none";
+  //   } else {
+  //     x.style.display = "block";
+  //   }
+  // }
+  
+  
   
