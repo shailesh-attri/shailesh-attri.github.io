844250b428fcda2aade8024a43512f1e0bc08b52
shailesh-attri <127712044+shailesh-attri@users.noreply.github.com>
2023-04-01 12:21:38 +0530

    yes

--- code.js
+++ code.js
@@ -18,7 +18,36 @@ window.addEventListener('scroll', function() {
     
     
   }
-  
+  function toggle_btn(){
+    const navList = document.querySelector('.dropdown-menu');
+    const menuBtn = document.querySelector('#menu-btn');
+   
+    menuBtn.classList.toggle('fa-times');
+    menuBtn.classList.toggle('fa-bars');
+    
+    navList.classList.toggle('active');
+    
+    
+  }
+  const hamburgerBtn = document.getElementById('menu-btn');
+const homeImg = document.querySelector('.my-home-img');
+
+hamburgerBtn.addEventListener('click', () => {
+  homeImg.classList.toggle('active');
+  alert("hello");
+});
+// function toggleTheme(){
+//   alert('hello');
+//   const Tog_div=document.querySelector('.tog_btn');  
+// const toggle=document.querySelector('.dropdown-menu');
+// const toggle_btn=document.querySelector('#menu-btn');
+
+// toggle.classList.toggle('open');
+// toggle_btn.classList.toggle('.fa-bars')
+// toggle_btn.classList.toggle('.fa-times')
+
+
+// }
 
 
 
@@ -36,10 +65,10 @@ window.addEventListener('scroll', function() {
 
 
 
-// toggleButton.addEventListener("click", function() {
-//   sidebar.classList.toggle("open");
-//   alert("hello");
-// });
+toggleButton.addEventListener("click", function() {
+  sidebar.classList.toggle("open");
+  alert("hello");
+});
 
 function red() {
   const body = document.querySelector('body');
@@ -97,6 +126,40 @@ textarea.addEventListener('focus', function() {
   this.style.borderColor = 'red';
   
 });
+
+
+function showLoader() {
+  document.getElementById("loader").style.display = "block";
+  // Add other loading actions here if needed
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("loader").style.display = "none";
+  // Add other actions after loading here if needed
+
+  window.onload = function() {
+    document.getElementById("loader").style.display = "none";
+  }
+});
+
+let my_button = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    my_button.style.display = "block";
+  } else {
+    my_button.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
 
 
 
